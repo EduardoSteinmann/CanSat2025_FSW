7c7fff6110521dea405984ae80aeda69c2f283a3
EduardoSteinmann <edward.steinmann@outlook.com>
2025-03-31 19:22:42 -0400

    Add fixed bmm150

--- Drivers/BMM150/Inc/BMM150SPI.h
+++ Drivers/BMM150/Inc/BMM150SPI.h
@@ -7,27 +7,20 @@
 
 typedef struct BMM150_dev
 {
-	bool ok;
+	int8_t error;
 	struct bmm150_dev device;
 } BMM150_dev;
 
 typedef struct BMM150_mag_data
 {
+	int16_t x;
+	int16_t y;
+	int16_t z;
 	int8_t error;
-	struct bmm150_mag_data data;
 } BMM150_mag_data;
 
-/**
-   * @brief  Initializes MS5607 Sensor
-   * @param  SPI Handle address
-   * @param  GPIO Port Definition
-   * @param  GPIO Pin
-   * @retval Initialization status:
-   *           - 0: Was not abe to communicate with sensor
-   *           - 1: MS5607_STATE_READY: Sensor initialized OK and ready to use
-   */
-BMM150_dev BMM150_spi_init(SPI_HandleTypeDef *spi_handle);
+BMM150_dev BMM150_spi_init(SPI_HandleTypeDef* spi_handle, GPIO_TypeDef* cs_port, uint16_t cs_gpio_pin);
 
-BMM150_mag_data BMM150_read_mag_data(BMM150_dev* const device);
+BMM150_mag_data BMM150_read_mag_data(BMM150_dev* device);
 
 #endif
