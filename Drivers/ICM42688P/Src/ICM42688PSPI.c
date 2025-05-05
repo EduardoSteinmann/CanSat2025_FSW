31d644cb2ec28ab6d2b4e984fd8c24e37468acad
EduardoSteinmann <edward.steinmann@outlook.com>
2025-05-05 18:53:46 -0400

    Forgot to push ICM tweaks

--- Drivers/ICM42688P/Src/ICM42688PSPI.c
+++ Drivers/ICM42688P/Src/ICM42688PSPI.c
@@ -1,56 +1,79 @@
 #include "ICM42688PSPI.h"
 #include "stm32g4xx_hal.h"
+#include <stdint.h>
 
-// #define ICM42688P_SPI &hspi1  // Change to match your SPI handle
-// #define ICM42688P_CS_LOW() HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET)
-// #define ICM42688P_CS_HIGH() HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET)
+static SPI_HandleTypeDef* hspi;
 
-static void icm42688p_write_reg(uint8_t reg, uint8_t data)
+/* Private GPIO CS Pin Variables */
+static GPIO_TypeDef* ChipSelect_GPIO_Port;
+static uint16_t ChipSelect_Pin;
+
+static void ICM42688P_disable_chip_select()
 {
-    uint8_t tx[2] = {reg, data};
-    ICM42688P_CS_LOW();
-    HAL_SPI_Transmit(ICM42688P_SPI, tx, 2, HAL_MAX_DELAY);
-    ICM42688P_CS_HIGH();
+    HAL_GPIO_WritePin(ChipSelect_GPIO_Port, ChipSelect_Pin, GPIO_PIN_RESET);
 }
 
-static uint8_t icm42688p_read_reg(uint8_t reg)
+static void ICM42688P_enable_chip_select()
+{
+    HAL_GPIO_WritePin(ChipSelect_GPIO_Port, ChipSelect_Pin, GPIO_PIN_SET);
+}
+
+static HAL_StatusTypeDef ICM42688P_write_reg(uint8_t reg, uint8_t data)
+{
+    uint8_t tx[2] = {reg, data};
+    ICM42688P_disable_chip_select();
+    HAL_SPI_Transmit(hspi, tx, 2, HAL_MAX_DELAY);
+    ICM42688P_enable_chip_select();
+    return HAL_OK;
+}
+
+static uint8_t ICM42688P_read_reg(uint8_t reg)
 {
     uint8_t tx = reg | 0x80;
     uint8_t rx = 0;
-    ICM42688P_CS_LOW();
-    HAL_SPI_Transmit(ICM42688P_SPI, &tx, 1, HAL_MAX_DELAY);
-    HAL_SPI_Receive(ICM42688P_SPI, &rx, 1, HAL_MAX_DELAY);
-    ICM42688P_CS_HIGH();
+    ICM42688P_disable_chip_select();
+    HAL_SPI_Transmit(hspi, &tx, 1, HAL_MAX_DELAY);
+    
+    HAL_SPI_Receive(hspi, &rx, 1, HAL_MAX_DELAY);
+    ICM42688P_enable_chip_select();
     return rx;
 }
 
-// Another init where device and pins can be set from input
-//(SPI_HandleTypeDef *spi_handle, GPIO_TypeDef* cs_port, uint16_t cs_gpio_pin)
-void icm42688p_init()
+uint8_t ICM42688P_init(SPI_HandleTypeDef* spi_handle, GPIO_TypeDef* chip_select_port, uint16_t chip_select_gpio_pin)
 {
+    hspi = spi_handle;
+    ChipSelect_GPIO_Port = chip_select_port;
+    ChipSelect_Pin = chip_select_gpio_pin;
+
     osDelay(10);
-    icm42688p_write_reg(0x4F, 0x04);  // Reset device
+    ICM42688P_write_reg(0x4F, 0x04);  // Reset device
     osDelay(10);
-    icm42688p_write_reg(0x11, 0x00);  // Power management
-    icm42688p_write_reg(0x10, 0x0F);  // Gyro and accel config
+    ICM42688P_write_reg(0x11, 0x00);  // Power management
+    ICM42688P_write_reg(0x10, 0x0F);  // Gyro and accel config
+
+    return 0;
 }
 
-void icm42688p_read_data(int16_t *accel, int16_t *gyro)
+ICM42688P_AccelData ICM42688P_read_data()
 {
+    ICM42688P_AccelData data = { 0 };
+
     uint8_t buffer[12];
-    ICM42688P_CS_LOW();
+    ICM42688P_disable_chip_select();
     //This register should be correct
     uint8_t reg = 0x1F | 0x80;
-    HAL_SPI_Transmit(ICM42688P_SPI, &reg, 1, HAL_MAX_DELAY);
-    HAL_SPI_Receive(ICM42688P_SPI, buffer, 12, HAL_MAX_DELAY);
-    ICM42688P_CS_HIGH();
+    HAL_SPI_Transmit(hspi, &reg, 1, HAL_MAX_DELAY);
+    HAL_SPI_Receive(hspi, &buffer, sizeof(buffer) / sizeof(buffer[0]), HAL_MAX_DELAY);
+    ICM42688P_enable_chip_select();
 
-    accel[0] = (buffer[0] << 8) | buffer[1];
-    accel[1] = (buffer[2] << 8) | buffer[3];
-    accel[2] = (buffer[4] << 8) | buffer[5];
-    gyro[0] = (buffer[6] << 8) | buffer[7];
-    gyro[1] = (buffer[8] << 8) | buffer[9];
-    gyro[2] = (buffer[10] << 8) | buffer[11];
+    data.accel_x = (buffer[0] << 8) | buffer[1];
+    data.accel_y = (buffer[2] << 8) | buffer[3];
+    data.accel_z = (buffer[4] << 8) | buffer[5];
+    data.gyro_x = (buffer[6] << 8) | buffer[7];
+    data.gyro_y = (buffer[8] << 8) | buffer[9];
+    data.gyro_z = (buffer[10] << 8) | buffer[11];
+
+    return data;
 }
 
 #endif
